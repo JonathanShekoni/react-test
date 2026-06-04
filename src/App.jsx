49678d3d0713d7f19da2b1e822ab021dd12de356
Jonathan Shekoni <jonathanshekoni1@gmail.com>
2026-06-03 20:10:16 -0400

    initial upload of folder

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
+import React from 'react';
+import HomePage from './Pages/HomePage';
+import MainLayout from './Layouts/MainLayout';
+import JobsPage from './Pages/JobsPage';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path = '/' element = {<MainLayout />}>
+    <Route index element ={<HomePage />} />
+    <Route path='/jobs' element ={<JobsPage />} />
+  </Route>
+  ) 
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App
---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar'
+import ViewAllJobs from '../components/ViewAllJobs'
+
+const MainLayout = () => {
+  return(
+  <>
+    <Navbar />
+    <Outlet /> 
+    <ViewAllJobs />
+  </>
+  );
+};
+
+export default MainLayout
